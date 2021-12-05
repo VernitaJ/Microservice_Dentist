dd2c8edf1a2aa4cddd79e2c7a5e769b20b3d38b4
Numan Korkmaz <numank@student.chalmers.se>
2021-12-05 16:05:15 +0100

    Remove express

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 require("dotenv").config();
-var express = require("express");
 var mongoose = require("mongoose");
-var cors = require("cors");
 var mqtt = require("mqtt");
-
-// Variables
-var port = process.env.NODE_DOCKER_PORT || 3001;
 
 // NOTE(numank): Replace mongoURI with
 //   `mongodb://localhost:${process.env.MONGODB_DOCKER_PORT}/dentistDB`
@@ -36,42 +31,3 @@ mongoose.connect(
     console.log(`Connected to MongoDB with URI: ${mongoURI}`);
   }
 );
-
-// Create Express app
-var app = express();
-// Parse requests of content-type 'application/json'
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Enable cross-origin resource sharing for frontend must be registered before api
-app.options("*", cors());
-app.use(cors());
-
-app.get("/", function (req, res) {
-  res.json({
-    message: "Default - Dentist Backend Microservice",
-  });
-});
-
-// Error handler (i.e., when exception is thrown) must be registered last
-var env = app.get("env");
-// eslint-disable-next-line no-unused-vars
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  var err_res = {
-    message: err.message,
-    error: {},
-  };
-  if (env === "development") {
-    // Return sensitive stack trace only in dev mode
-    err_res["error"] = err.stack;
-  }
-  res.status(err.status || 500);
-  res.json(err_res);
-});
-
-app.listen(port, function (err) {
-  if (err) throw err;
-  console.log(`Express server listening on port ${port}, in ${env} mode`);
-});
-
-module.exports = app;
