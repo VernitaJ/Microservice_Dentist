58fbd3aae9923bca33a3dc7f4372575bda169243
Numan Korkmaz <numank@student.chalmers.se>
2021-12-05 17:52:45 +0100

    Import dentists on request

---
+++ models/dentists.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const dentistSchema = new Schema({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  owner: { type: String },
+  dentists: { type: Number, required: true },
+  address: { type: String },
+  city: { type: String },
+  coordinate: {
+    type: { type: Object },
+    longitude: String,
+    latitude: String,
+  },
+  openingHours: {
+    monday: { type: String, required: true },
+    tuesday: { type: String, required: true },
+    wednesday: { type: String, required: true },
+    thursday: { type: String, required: true },
+    friday: { type: String, required: true },
+  },
+});
+
+module.exports = mongoose.model("Dentist", dentistSchema);
